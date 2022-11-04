236cdfacc95a2df0cff9c704ce56fd6dff0c9ac0
moaz-wego <moaz@wego.com>
2022-11-04 14:57:45 +0500

    FIRST COMMIT

---
+++ app/src/main/java/com/example/android/marsphotos/BindingAdapters.kt
@@ -0,0 +1,54 @@
+package com.example.android.marsphotos
+
+import android.util.Log
+import android.view.View
+import android.widget.ImageView
+import androidx.core.net.toUri
+import androidx.databinding.BindingAdapter
+import androidx.recyclerview.widget.RecyclerView
+import coil.load
+import com.example.android.marsphotos.network.MarsPhoto
+import com.example.android.marsphotos.overview.MarsApiStatus
+import com.example.android.marsphotos.overview.PhotoGridAdapter
+
+@BindingAdapter("imageUrl")
+fun bindImage(imgView: ImageView, imgUrl: String?) {
+    imgUrl?.let {
+//        imgView.context?.let {
+//
+//        }
+
+
+        val imgUri = imgUrl.toUri().buildUpon().scheme("https").build()
+        imgView.load(imgUri){
+            placeholder(R.drawable.loading_animation)
+            error(R.drawable.ic_broken_image)
+        }
+    }
+}
+@BindingAdapter("listData")
+fun bindRecyclerView(recyclerView: RecyclerView,
+                     data: List<MarsPhoto>?) {
+    Log.d("MAAZ","HERE1")
+    val adapter = recyclerView.adapter as PhotoGridAdapter
+    adapter.submitList(data)
+}
+@BindingAdapter("marsApiStatus")
+fun bindStatus(img:ImageView,status:MarsApiStatus)
+{
+    when(status)
+    {
+        MarsApiStatus.LOADING->{
+            img.visibility= View.VISIBLE
+            img.setImageResource(R.drawable.loading_animation)
+        }
+        MarsApiStatus.ERROR->{
+            img.visibility=View.VISIBLE
+            img.setImageResource(R.drawable.ic_connection_error)
+        }
+        MarsApiStatus.DONE->{
+            img.visibility=View.GONE
+
+        }
+    }
+}
---
+++ app/src/main/java/com/example/android/marsphotos/overview/PhotoGridAdapter.kt
@@ -0,0 +1,62 @@
+package com.example.android.marsphotos.overview
+
+import android.icu.lang.UCharacter.GraphemeClusterBreak.T
+import android.util.Log
+import android.view.LayoutInflater
+import android.view.ViewGroup
+import androidx.recyclerview.widget.DiffUtil
+import androidx.recyclerview.widget.ListAdapter
+import androidx.recyclerview.widget.RecyclerView
+import com.bumptech.glide.Glide
+import com.example.android.marsphotos.R
+import com.example.android.marsphotos.databinding.GridViewItemBinding
+import com.example.android.marsphotos.network.MarsPhoto
+
+class PhotoGridAdapter : ListAdapter<MarsPhoto,
+        PhotoGridAdapter.MarsPhotoViewHolder>(DiffCallback) {
+
+    var counter=0;
+    class MarsPhotoViewHolder(private var binding: GridViewItemBinding
+    ): RecyclerView.ViewHolder(binding.root)
+    {
+
+        fun bind(MarsPhoto: MarsPhoto) {
+
+            //binding.photo = MarsPhoto
+            //binding.executePendingBindings()
+            Glide.with(binding.root.context)
+                .load(MarsPhoto.imgSrcUrl)
+                .placeholder(R.drawable.loading_animation)
+                .error(R.drawable.ic_broken_image)
+                .into(binding.marsImage)
+
+        }
+
+    }
+
+    companion object DiffCallback : DiffUtil.ItemCallback<MarsPhoto>() {
+        override fun areItemsTheSame(oldItem: MarsPhoto, newItem: MarsPhoto): Boolean {
+            Log.d("MAAZ"," in ITEM SAME")
+            return oldItem.id == newItem.id
+        }
+
+        override fun areContentsTheSame(oldItem: MarsPhoto, newItem: MarsPhoto): Boolean {
+            Log.d("MAAZ"," in CONTENT SAME")
+            return oldItem.imgSrcUrl == newItem.imgSrcUrl
+        }
+    }
+    override fun onCreateViewHolder(parent: ViewGroup, viewType: Int): PhotoGridAdapter.MarsPhotoViewHolder {
+        Log.d("MAAZ","$counter in Create")
+        counter++
+         return MarsPhotoViewHolder(GridViewItemBinding.inflate(
+            LayoutInflater.from(parent.context)))
+    }
+
+    override fun onBindViewHolder(holder: PhotoGridAdapter.MarsPhotoViewHolder, position: Int) {
+        val marsPhoto = getItem(position)
+        Log.d("MAAZ"," in Bind")
+        holder.bind(marsPhoto)
+    }
+
+
+}
